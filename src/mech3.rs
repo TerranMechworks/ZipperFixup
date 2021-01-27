c14ade91dacdbb3b2c44dc0649474d6d9a57bb8c
Toby Fleming <tobywf@users.noreply.github.com>
2021-01-26 18:36:26 -0800

    Idiomize Rust code a bit

--- src/mech3.rs
+++ src/mech3.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 macro_rules! addr_as_fn {
     ($address:expr, $t:ty) => {
-        std::mem::transmute::<*const (), $t>($address as _)
+        unsafe { std::mem::transmute::<*const (), $t>($address as _) }
     };
 }
 
@@ -18,32 +18,33 @@ fn clamp(min: i32, max: i32, value: i32) -> i32 {
 }
 
 fn clamp_to_screen_space(x: i32, y: i32) -> (i32, i32) {
-    unsafe {
-        let x_out = clamp(0, (*SCREEN_WIDTH_PX) - 1, x);
-        let y_out = clamp(0, (*SCREEN_HEIGHT_PX) - 1, y);
-        (x_out, y_out)
-    }
+    let x_res = unsafe { *SCREEN_WIDTH_PX };
+    let y_res = unsafe { *SCREEN_HEIGHT_PX };
+    let x_out = clamp(0, x_res - 1, x);
+    let y_out = clamp(0, y_res - 1, y);
+    (x_out, y_out)
 }
 
 // Function pointer in game we can overwrite
 type DrawLineFn = extern "fastcall" fn(*mut u16, i32, i32, i32, i32, i16);
 const DRAW_LINE_FN_PTR: *mut DrawLineFn = 0x008008c0 as *mut DrawLineFn;
+const DRAW_LINE_ADDR: *const () = 0x00563b80 as _;
+
 type DrawDashedLineFn = extern "fastcall" fn(*mut u16, i32, i32, i32, i32, i16, i32);
 const DRAW_DASHED_LINE_FN_PTR: *mut DrawDashedLineFn = 0x008008c4 as *mut DrawDashedLineFn;
-
-fn install_hooks_impl() {
-    // TODO Really unsafe.
-    // Would it be better to replace the start of the original functions witha jmp?
-    thread::sleep(Duration::from_millis(5000));
-
-    unsafe {
-        *DRAW_LINE_FN_PTR = draw_line;
-        *DRAW_DASHED_LINE_FN_PTR = draw_dashed_line;
-    }
-}
+const DRAW_DASHED_LINE_ADDR: *const () = 0x00563c50 as _;
 
 pub fn install_hooks() {
-    let _handle = thread::spawn(install_hooks_impl);
+    let _handle = thread::spawn(|| {
+        // TODO Really unsafe.
+        // Would it be better to replace the start of the original functions with a jmp?
+        thread::sleep(Duration::from_millis(5000));
+
+        unsafe {
+            *DRAW_LINE_FN_PTR = draw_line;
+            *DRAW_DASHED_LINE_FN_PTR = draw_dashed_line;
+        }
+    });
 }
 
 extern "fastcall" fn draw_line(
@@ -54,12 +55,11 @@ extern "fastcall" fn draw_line(
     y2: i32,
     color: i16,
 ) {
-    let (x1_out, y1_out) = clamp_to_screen_space(x1, y1);
-    let (x2_out, y2_out) = clamp_to_screen_space(x2, y2);
+    let (x1, y1) = clamp_to_screen_space(x1, y1);
+    let (x2, y2) = clamp_to_screen_space(x2, y2);
 
-    const DRAW_LINE_ADDR: *const () = 0x00563b80 as *const ();
-    let func = unsafe { addr_as_fn!(DRAW_LINE_ADDR, DrawLineFn) };
-    func(screen_buffer, x1_out, y1_out, x2_out, y2_out, color);
+    let func = addr_as_fn!(DRAW_LINE_ADDR, DrawLineFn);
+    func(screen_buffer, x1, y1, x2, y2, color);
 }
 
 extern "fastcall" fn draw_dashed_line(
@@ -71,18 +71,9 @@ extern "fastcall" fn draw_dashed_line(
     color: i16,
     sections: i32,
 ) {
-    let (x1_out, y1_out) = clamp_to_screen_space(x1, y1);
-    let (x2_out, y2_out) = clamp_to_screen_space(x2, y2);
+    let (x1, y1) = clamp_to_screen_space(x1, y1);
+    let (x2, y2) = clamp_to_screen_space(x2, y2);
 
-    const DRAW_DASHED_LINE_ADDR: *const () = 0x00563c50 as *const ();
-    let func = unsafe { addr_as_fn!(DRAW_DASHED_LINE_ADDR, DrawDashedLineFn) };
-    func(
-        screen_buffer,
-        x1_out,
-        y1_out,
-        x2_out,
-        y2_out,
-        color,
-        sections,
-    );
+    let func = addr_as_fn!(DRAW_DASHED_LINE_ADDR, DrawDashedLineFn);
+    func(screen_buffer, x1, y1, x2, y2, color, sections);
 }
