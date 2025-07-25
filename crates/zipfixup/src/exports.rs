0e2e0485fcd09719b7f445ded32a80d56a4134af
Toby Fleming <sourcecode@tobywf.com>
2025-07-25 16:52:20 -0700

    Don't require ordinals for forwarding

--- crates/zipfixup/src/exports.rs
+++ crates/zipfixup/src/exports.rs
@@ -15,7 +15,7 @@ macro_rules! forwarded_override {
 }
 
 macro_rules! forwarded_kernel32 {
-    ($exp_name:expr, $ordinal:expr) => {
+    ($exp_name:expr) => {
         global_asm!(concat!(
             ".ascii \" -export:",
             $exp_name,
@@ -37,66 +37,66 @@ macro_rules! forwarded_kernel32 {
 forwarded_override!("GetTickCount", "get_tick_count", 469);
 
 // Kernel32 functions for MW and others
-forwarded_kernel32!("ReadFile", 679);
-forwarded_kernel32!("CreateFileA", 80);
-forwarded_kernel32!("FindNextFileA", 218);
-forwarded_kernel32!("CloseHandle", 50);
-forwarded_kernel32!("DeleteFileA", 130);
-forwarded_kernel32!("SetFilePointer", 778);
-forwarded_kernel32!("FindFirstFileA", 209);
-forwarded_kernel32!("ExitProcess", 183);
-forwarded_kernel32!("MoveFileA", 609);
-forwarded_kernel32!("FormatMessageA", 236);
-forwarded_kernel32!("Sleep", 836);
-forwarded_kernel32!("LocalFree", 591);
-forwarded_kernel32!("GetCommandLineA", 266);
-forwarded_kernel32!("SetThreadPriority", 819);
-forwarded_kernel32!("SetCurrentDirectoryA", 763);
-forwarded_kernel32!("ResetEvent", 704);
-forwarded_kernel32!("CreateEventA", 76);
-forwarded_kernel32!("CreateThread", 109);
-forwarded_kernel32!("WaitForSingleObject", 896);
-forwarded_kernel32!("OutputDebugStringA", 639);
-forwarded_kernel32!("SetEvent", 773);
-forwarded_kernel32!("GetDriveTypeA", 332);
-forwarded_kernel32!("GetTempPathA", 460);
-forwarded_kernel32!("FreeLibrary", 241);
-forwarded_kernel32!("GetProcAddress", 409);
-forwarded_kernel32!("GetLogicalDriveStringsA", 366);
-forwarded_kernel32!("GetFileSize", 348);
-forwarded_kernel32!("GetFileTime", 350);
-forwarded_kernel32!("LoadLibraryA", 581);
-forwarded_kernel32!("lstrlenA", 953);
-forwarded_kernel32!("GetVersionExA", 479);
-forwarded_kernel32!("GlobalMemoryStatus", 506);
-forwarded_kernel32!("QueryPerformanceFrequency", 662);
-forwarded_kernel32!("GetThreadPriority", 465);
-forwarded_kernel32!("QueryPerformanceCounter", 661);
-forwarded_kernel32!("InitializeCriticalSection", 537);
-forwarded_kernel32!("DeleteCriticalSection", 128);
-forwarded_kernel32!("GetCurrentThread", 318);
-forwarded_kernel32!("EnterCriticalSection", 151);
-forwarded_kernel32!("ReleaseMutex", 692);
-forwarded_kernel32!("LeaveCriticalSection", 580);
-forwarded_kernel32!("GetModuleHandleA", 375);
-forwarded_kernel32!("GetStartupInfoA", 431);
-forwarded_kernel32!("CreateMutexA", 93);
-forwarded_kernel32!("GetLastError", 361);
-forwarded_kernel32!("WriteFile", 913);
+forwarded_kernel32!("ReadFile"); // ordinal: 679
+forwarded_kernel32!("CreateFileA"); // ordinal: 80
+forwarded_kernel32!("FindNextFileA"); // ordinal: 218
+forwarded_kernel32!("CloseHandle"); // ordinal: 50
+forwarded_kernel32!("DeleteFileA"); // ordinal: 130
+forwarded_kernel32!("SetFilePointer"); // ordinal: 778
+forwarded_kernel32!("FindFirstFileA"); // ordinal: 209
+forwarded_kernel32!("ExitProcess"); // ordinal: 183
+forwarded_kernel32!("MoveFileA"); // ordinal: 609
+forwarded_kernel32!("FormatMessageA"); // ordinal: 236
+forwarded_kernel32!("Sleep"); // ordinal: 836
+forwarded_kernel32!("LocalFree"); // ordinal: 591
+forwarded_kernel32!("GetCommandLineA"); // ordinal: 266
+forwarded_kernel32!("SetThreadPriority"); // ordinal: 819
+forwarded_kernel32!("SetCurrentDirectoryA"); // ordinal: 763
+forwarded_kernel32!("ResetEvent"); // ordinal: 704
+forwarded_kernel32!("CreateEventA"); // ordinal: 76
+forwarded_kernel32!("CreateThread"); // ordinal: 109
+forwarded_kernel32!("WaitForSingleObject"); // ordinal: 896
+forwarded_kernel32!("OutputDebugStringA"); // ordinal: 639
+forwarded_kernel32!("SetEvent"); // ordinal: 773
+forwarded_kernel32!("GetDriveTypeA"); // ordinal: 332
+forwarded_kernel32!("GetTempPathA"); // ordinal: 460
+forwarded_kernel32!("FreeLibrary"); // ordinal: 241
+forwarded_kernel32!("GetProcAddress"); // ordinal: 409
+forwarded_kernel32!("GetLogicalDriveStringsA"); // ordinal: 366
+forwarded_kernel32!("GetFileSize"); // ordinal: 348
+forwarded_kernel32!("GetFileTime"); // ordinal: 350
+forwarded_kernel32!("LoadLibraryA"); // ordinal: 581
+forwarded_kernel32!("lstrlenA"); // ordinal: 953
+forwarded_kernel32!("GetVersionExA"); // ordinal: 479
+forwarded_kernel32!("GlobalMemoryStatus"); // ordinal: 506
+forwarded_kernel32!("QueryPerformanceFrequency"); // ordinal: 662
+forwarded_kernel32!("GetThreadPriority"); // ordinal: 465
+forwarded_kernel32!("QueryPerformanceCounter"); // ordinal: 661
+forwarded_kernel32!("InitializeCriticalSection"); // ordinal: 537
+forwarded_kernel32!("DeleteCriticalSection"); // ordinal: 128
+forwarded_kernel32!("GetCurrentThread"); // ordinal: 318
+forwarded_kernel32!("EnterCriticalSection"); // ordinal: 151
+forwarded_kernel32!("ReleaseMutex"); // ordinal: 692
+forwarded_kernel32!("LeaveCriticalSection"); // ordinal: 580
+forwarded_kernel32!("GetModuleHandleA"); // ordinal: 375
+forwarded_kernel32!("GetStartupInfoA"); // ordinal: 431
+forwarded_kernel32!("CreateMutexA"); // ordinal: 93
+forwarded_kernel32!("GetLastError"); // ordinal: 361
+forwarded_kernel32!("WriteFile"); // ordinal: 913
 
 // Kernel32 functions for RC
-forwarded_kernel32!("InterlockedDecrement", 541);
-forwarded_kernel32!("InterlockedIncrement", 545);
-forwarded_kernel32!("CompareFileTime", 55);
-forwarded_kernel32!("WaitForMultipleObjects", 894);
-forwarded_kernel32!("GetSystemDefaultLangID", 440);
-forwarded_kernel32!("CreateDirectoryA", 72);
-forwarded_kernel32!("GetCurrentDirectoryA", 314);
+forwarded_kernel32!("InterlockedDecrement"); // ordinal: 541
+forwarded_kernel32!("InterlockedIncrement"); // ordinal: 545
+forwarded_kernel32!("CompareFileTime"); // ordinal: 55
+forwarded_kernel32!("WaitForMultipleObjects"); // ordinal: 894
+forwarded_kernel32!("GetSystemDefaultLangID"); // ordinal: 440
+forwarded_kernel32!("CreateDirectoryA"); // ordinal: 72
+forwarded_kernel32!("GetCurrentDirectoryA"); // ordinal: 314
 
 // Kernel32 functions for PM
-forwarded_kernel32!("FindClose", 205);
-forwarded_kernel32!("LoadResource", 586);
-forwarded_kernel32!("LockResource", 600);
-forwarded_kernel32!("SizeofResource", 835);
-forwarded_kernel32!("FindResourceExA", 225);
-forwarded_kernel32!("EnumResourceNamesA", 164);
+forwarded_kernel32!("FindClose"); // ordinal: 205
+forwarded_kernel32!("LoadResource"); // ordinal: 586
+forwarded_kernel32!("LockResource"); // ordinal: 600
+forwarded_kernel32!("SizeofResource"); // ordinal: 835
+forwarded_kernel32!("FindResourceExA"); // ordinal: 225
+forwarded_kernel32!("EnumResourceNamesA"); // ordinal: 164
