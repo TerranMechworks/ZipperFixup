b8e52b530cb1a8a6b67d48cd75734b34c35ccaef
Toby Fleming <sourcecode@tobywf.com>
2025-07-25 13:41:26 -0700

    Output timestamps

--- crates/zipfixup/src/dbg.rs
+++ crates/zipfixup/src/dbg.rs
@@ -1,9 +1,20 @@
 use ::winapi::um::debugapi::{OutputDebugStringA, OutputDebugStringW};
 
-/// Adds zero-termination.
 #[expect(dead_code, reason = "test me!")]
-pub(crate) unsafe fn encode_unicode(s: &str) {
-    let v: Vec<u16> = s.encode_utf16().chain(Some(0)).collect();
+pub(crate) fn encode_unicode(msg: &str) {
+    let now = time::OffsetDateTime::now_utc();
+    let s = format!(
+        "[ZF {:04}-{:02}-{:02}T{:02}:{:02}:{:02}.{:03}Z] {msg}\0",
+        now.year(),
+        now.month(),
+        now.day(),
+        now.hour(),
+        now.minute(),
+        now.second(),
+        now.millisecond(),
+    );
+
+    let v: Vec<u16> = s.encode_utf16().collect();
     let p: *const u16 = v.as_ptr();
     unsafe { OutputDebugStringW(p) };
     // paranoia: ensure `v` is valid until after `OutputDebugStringW`
@@ -11,9 +22,23 @@ pub(crate) unsafe fn encode_unicode(s: &str) {
 }
 
 /// WARNING: message must be ASCII!
-pub(crate) unsafe fn encode_ascii(s: &str) {
+pub(crate) unsafe fn encode_ascii(msg: &str) {
+    let now = time::OffsetDateTime::now_utc();
+    let s = format!(
+        "[ZF {:04}-{:02}-{:02}T{:02}:{:02}:{:02}.{:03}Z] {msg}\0",
+        now.year(),
+        now.month(),
+        now.day(),
+        now.hour(),
+        now.minute(),
+        now.second(),
+        now.millisecond(),
+    );
+
     let p: *const i8 = s.as_ptr() as *const i8;
     unsafe { OutputDebugStringA(p) };
+    // paranoia: ensure `s` is valid until after `OutputDebugStringW`
+    drop(s);
 }
 
 /// WARNING: message must be ASCII!
@@ -24,13 +49,13 @@ pub(crate) unsafe fn encode_ascii(s: &str) {
 ///
 /// I think I tested that though, and `OutputDebugStringW` didn't work?
 macro_rules! output {
-    ($fmt:literal) => {{
-        let msg: &str = concat!("[ZIPFIXUP] ", $fmt, "\0");
-        unsafe { $crate::dbg::encode_ascii(msg) };
-    }};
-    ($fmt:literal, $($args:expr),+ $(,)?) => {{
-        let msg: String = format!(concat!("[ZIPFIXUP] ", $fmt, "\0"), $($args,)+);
+    ($fmt:literal $(, $args:expr)* $(,)?) => {{
+        let msg: String = format!($fmt $(, $args)*);
         unsafe { $crate::dbg::encode_ascii(&msg) };
+    }};
+    (u $fmt:literal $(, $args:expr)* $(,)?) => {{
+        let msg: String = format!($fmt $(, $args)*);
+        $crate::dbg::encode_unicode(&msg);
     }};
 }
 pub(crate) use output;
