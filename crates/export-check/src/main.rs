3a90c3542a401853b04222040beba5eae6ea695d
Toby Fleming <sourcecode@tobywf.com>
2025-07-26 02:16:39 -0700

    Allow forwarding of other DLLs

--- crates/export-check/src/main.rs
+++ crates/export-check/src/main.rs
@@ -1,86 +1,41 @@
-use object::read::pe::{ExportTarget, PeFile32};
+use object::read::pe::{Export, ExportTarget, PeFile32};
 use std::collections::HashSet;
+use std::fmt;
 
 const DEF_FILE: &str = include_str!("../../zipfixup/exports.def");
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let (expected_exports, expected_forwards) = parse_def_file()?;
 
-    let contents = std::fs::read("target/i686-pc-windows-gnu/release/zipfixup.dll")?;
-    let data = contents.as_slice();
+    for path in std::env::args().skip(1) {
+        println!("=== {} ===", path);
 
-    let pe = PeFile32::parse(data)?;
-    let table = pe.export_table()?.ok_or("no export table")?;
-    let exported = table.exports()?;
+        let contents = std::fs::read(path)?;
+        let data = contents.as_slice();
+        let pe = PeFile32::parse(data)?;
+        let table = pe.export_table()?.ok_or("no export table")?;
+        let exported = table.exports()?;
 
-    let mut exports = Vec::new();
-    let mut forwards = Vec::new();
-    for export in &exported {
-        let name = export
-            .name
-            .ok_or_else(|| format!("export has no name: {export:?}"))?;
-        let name = str::from_utf8(name)?;
-
-        match export.target {
-            ExportTarget::ForwardByName(module, forward) => {
-                if module != b"KERNEL32" {
-                    return Err(format!("expected forward to KERNEL32: {export:?}").into());
-                }
-                if name.as_bytes() != forward {
-                    return Err(format!(
-                        "forward name mismatch `{name}` != `{}`",
-                        forward.escape_ascii()
-                    )
-                    .into());
-                }
-                forwards.push(name);
-            }
-            ExportTarget::Address(_addr) => {
-                exports.push(name);
-            }
-            ExportTarget::ForwardByOrdinal(module, _ordinal) => {
-                if module != b"KERNEL32" {
-                    return Err(format!("expected forward to KERNEL32: {export:?}").into());
-                }
-                return Err(format!("unexpected forward by ordinal: {export:?}").into());
-            }
-        }
-    }
-
-    let mut actual_exports: HashSet<&str> = exports.iter().copied().collect();
-    let mut actual_forwards: HashSet<&str> = forwards.iter().copied().collect();
-
-    exports.sort();
-    forwards.sort();
-
-    for name in exports.iter().copied() {
-        println!("{name}");
-    }
-
-    for name in forwards.iter().copied() {
-        println!("KERNEL32.{name}");
-    }
-
-    for expected in expected_exports {
-        if !actual_exports.remove(&expected) {
-            return Err(format!("missing export `{expected}`").into());
-        }
-    }
-
-    for expected in expected_forwards {
-        if !actual_forwards.remove(&expected) {
-            return Err(format!("missing forward `{expected}`").into());
-        }
-    }
-
-    if !actual_forwards.is_empty() {
-        return Err(format!("unexpected forwards: {actual_forwards:?}").into());
+        validate_exports(&exported, &expected_exports, &expected_forwards)?;
     }
 
     Ok(())
 }
 
-fn parse_def_file() -> Result<(Vec<&'static str>, Vec<&'static str>), String> {
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Forward<'a> {
+    export: &'a str,
+    module: &'a str,
+    forward: &'a str,
+}
+
+impl fmt::Display for Forward<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}={}.{}", self.export, self.module, self.forward)
+    }
+}
+
+fn parse_def_file() -> Result<(Vec<&'static str>, Vec<Forward<'static>>), String> {
     let mut it = DEF_FILE.lines();
     let line = it.next().unwrap();
     if line != "LIBRARY ZIPFIXUP" {
@@ -100,18 +55,89 @@ fn parse_def_file() -> Result<(Vec<&'static str>, Vec<&'static str>), String> {
             continue;
         }
 
-        match line.split_once("=KERNEL32.") {
-            Some((fwd, name)) => {
-                if fwd != name {
-                    return Err(format!("expected `{fwd}` == `{name}`"));
-                }
-                forwards.push(name);
+        let parse: Vec<&str> = line.split(&['=', '.']).collect();
+        match &parse[..] {
+            [export, module, forward] => {
+                forwards.push(Forward {
+                    export,
+                    module,
+                    forward,
+                });
             }
-            None => {
+            _ => {
                 exports.push(line);
             }
         }
     }
 
     Ok((exports, forwards))
+}
+
+fn validate_exports(
+    exported: &[Export<'_>],
+    expected_exports: &[&'static str],
+    expected_forwards: &[Forward<'static>],
+) -> Result<(), String> {
+    let mut exports = Vec::new();
+    let mut forwards = Vec::new();
+    for export in exported {
+        let name = export
+            .name
+            .ok_or_else(|| format!("export has no name: {export:?}"))?;
+        let name =
+            str::from_utf8(name).map_err(|_e| format!("export name is not unicode: {export:?}"))?;
+
+        match export.target {
+            ExportTarget::ForwardByName(module, forward) => {
+                let module = str::from_utf8(module)
+                    .map_err(|_e| format!("export module is not unicode: {export:?}"))?;
+                let forward = str::from_utf8(forward)
+                    .map_err(|_e| format!("export forward is not unicode: {export:?}"))?;
+
+                forwards.push(Forward {
+                    export: name,
+                    module,
+                    forward,
+                });
+            }
+            ExportTarget::Address(_addr) => {
+                exports.push(name);
+            }
+            ExportTarget::ForwardByOrdinal(_module, _ordinal) => {
+                return Err(format!("unexpected forward by ordinal: {export:?}"));
+            }
+        }
+    }
+
+    let mut actual_exports: HashSet<&str> = exports.iter().copied().collect();
+    let mut actual_forwards: HashSet<Forward<'_>> = forwards.iter().copied().collect();
+
+    exports.sort();
+    forwards.sort();
+
+    for name in exports.iter().copied() {
+        println!("{name}");
+    }
+
+    for forward in forwards.iter().copied() {
+        println!("{forward}");
+    }
+
+    for expected in expected_exports {
+        if !actual_exports.remove(expected) {
+            return Err(format!("missing export `{expected}`"));
+        }
+    }
+
+    for expected in expected_forwards {
+        if !actual_forwards.remove(expected) {
+            return Err(format!("missing forward `{expected}`"));
+        }
+    }
+
+    if !actual_forwards.is_empty() {
+        return Err(format!("unexpected forwards: {actual_forwards:?}"));
+    }
+
+    Ok(())
 }
--- crates/zipfixup/build.rs
+++ crates/zipfixup/build.rs
@@ -1,8 +1,25 @@
 fn main() {
+    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
+
     let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let mut path = std::path::PathBuf::from(path);
     path.push("exports.def");
-    println!("cargo::warning={}", path.display());
-    println!("cargo::rerun-if-changed={}", path.display());
-    println!("cargo::rustc-link-arg-cdylib={}", path.display());
+    let path = format!("{}", path.display());
+
+    println!("cargo::warning={path}");
+    println!("cargo::rerun-if-changed={path}");
+
+    match env.as_str() {
+        "gnu" => {
+            println!("cargo::warning=GNU");
+            println!("cargo::rustc-link-arg-cdylib={path}");
+        }
+        "msvc" => {
+            println!("cargo::warning=MSVC");
+            println!("cargo::rustc-link-arg-cdylib=/DEF:{path}");
+        }
+        _ => {
+            println!("cargo::warning=unknown env `{env}`");
+        }
+    }
 }
